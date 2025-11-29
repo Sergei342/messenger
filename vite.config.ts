3b74d2c63e455f8af32befd4173bba6e86877a66
sergei <dukmas08@gmail.com>
2025-11-29 22:47:39 +0300

    fix

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        signUp: resolve(__dirname, 'src/sign-up.html'),
+        messenger: resolve(__dirname, 'src/messenger.html'),
+        settings: resolve(__dirname, 'src/settings.html'),
+        error404: resolve(__dirname, 'src/404.html'),
+        error500: resolve(__dirname, 'src/500.html'),
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  publicDir: resolve(__dirname, 'static'),
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@components': resolve(__dirname, 'src/components'),
+      '@pages': resolve(__dirname, 'src/pages'),
+      '@utils': resolve(__dirname, 'src/utils'),
+      '@services': resolve(__dirname, 'src/services'),
+      '@core': resolve(__dirname, 'src/core'),
+    },
+  },
+});
