e7e76c16eecc8b105e52e9d30afd3d8db0788c5b
sergei <dukmas08@gmail.com>
2025-12-01 20:15:42 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import string from 'vite-plugin-string'; // ← Изменили: убрали { }
+import string from 'vite-plugin-string';
 
 export default defineConfig({
   plugins: [
@@ -16,11 +16,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
-        signUp: resolve(__dirname, 'src/sign-up.html'),
-        messenger: resolve(__dirname, 'src/messenger.html'),
-        settings: resolve(__dirname, 'src/settings.html'),
-        error404: resolve(__dirname, 'src/404.html'),
-        error500: resolve(__dirname, 'src/500.html'),
       },
     },
   },
@@ -42,4 +37,3 @@ export default defineConfig({
     },
   },
 });
-
